a860027d61fa794cca9aef1fa2d317462e624799
Jeremy Malczyk <jmalczyk@users.noreply.github.com>
2018-07-19 11:13:17 -0400

    Create splitshp.sh

---
+++ splitshp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Split a shapefile by attribute values.
+# Usage: splitshp.sh shapefilename.shp some_field_name
+
+
+if [[ -z "$1" || -z "$2" ]]; then
+  echo "Usage: split_by_attr.sh some_shapefile_name.shp some_field_name"
+else
+   SHAPEFILE=$1
+   DATASET=${SHAPEFILE/\.shp/}
+   FIELD_NAME=$2
+   ogr2ogr  -sql "SELECT DISTINCT $FIELD_NAME FROM $DATASET" -f 'CSV' temp.csv $SHAPEFILE
+   for FIELD_VALUE in $(tail -n +2 temp.csv);
+   do
+      echo  Processing $FIELD_VALUE ...
+      ogr2ogr -sql "SELECT * FROM $DATASET where $FIELD_NAME = '$FIELD_VALUE'" -f 'ESRI Shapefile' ${DATASET}_${FIELD_VALUE}.shp $SHAPEFILE
+   done
+fi
+rm temp.csv
