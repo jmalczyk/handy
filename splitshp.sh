cfc6f5be57159c423541bd3d7b439a25e045cc05
Jeremy Malczyk <jmalczyk@users.noreply.github.com>
2018-07-19 16:09:17 -0400

    Set newline seperator

--- splitshp.sh
+++ splitshp.sh
@@ -2,6 +2,7 @@
 # Split a shapefile by attribute values.
 # Usage: splitshp.sh shapefilename.shp some_field_name
 
+IFS=$"\n"
 
 if [[ -z "$1" || -z "$2" ]]; then
   echo "Usage: split_by_attr.sh some_shapefile_name.shp some_field_name"
